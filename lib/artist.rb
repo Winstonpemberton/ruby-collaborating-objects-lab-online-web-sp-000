6de25716735477a1c43aca3c6a76c47e933935d1
Winston Pemberton <winstonpemberton@gmail.com>
2020-01-06 22:44:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,13 +13,13 @@ class Artist
   end
 
   def songs
-    #get all songs from songs class
-    #iterate through songs
-    #take out songs with the same artist
-    #return that array
     Song.all.select do |song|
       song.artist == self
     end
+  end
+
+  def add_song
+    
   end
 
 
