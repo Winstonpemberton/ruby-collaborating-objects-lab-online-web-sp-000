d4caf5a6a146921596b6f19272b927c68b7e8d66
Winston Pemberton <winstonpemberton@gmail.com>
2020-01-06 23:07:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,7 +26,7 @@ class Artist
   def self.find_or_create_by_name(artist)
     #try to find the artist in @@all
     #if it can't find it create a new artist
-    new_artist
+    new_artist = nil
 
     all.select do |artist_array|
       if artist_array == artist
